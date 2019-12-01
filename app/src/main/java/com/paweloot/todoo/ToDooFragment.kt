75abfbdd4ecb99ce7ea3a9b56da7d37b80b522f7
Paweł Banaś <paweloot@gmail.com>
2019-12-01 10:48:48 +0100

    Work on todo list design

--- app/src/main/java/com/paweloot/todoo/ToDooFragment.kt
+++ app/src/main/java/com/paweloot/todoo/ToDooFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -15,6 +16,7 @@ class ToDooFragment : Fragment() {
 
     private lateinit var toDooViewModel: ToDooViewModel
 
+    private lateinit var noteTitle: TextView
     private lateinit var toDooList: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +33,12 @@ class ToDooFragment : Fragment() {
         val view =
             inflater.inflate(R.layout.fragment_to_doo, container, false)
 
+        noteTitle = view.findViewById(R.id.note_title)
         toDooList = view.findViewById(R.id.to_doo_list)
-        toDooList.layoutManager = LinearLayoutManager(context)
 
+        noteTitle.text = "TODO"
+
+        toDooList.layoutManager = LinearLayoutManager(context)
         toDooViewModel.notes.observe(
             viewLifecycleOwner,
             Observer<List<ToDooNote>> { notes ->
